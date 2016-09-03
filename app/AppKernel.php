5ab2a16062b8cbe22740575d6786efd8209fd705
marcin <unvoid.pl@gmail.com>
2016-09-03 13:34:41 +0200

    redis; gedmo doctrine; basic configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // additional vendor bundles
+            new Snc\RedisBundle\SncRedisBundle(),
+
+            // project bundles
             new ElasticBundle\ElasticBundle(),
         ];
 
