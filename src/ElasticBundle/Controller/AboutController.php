acd949541ebdff32df9ead4c75d628e5327b664e
any1 <any1>
2018-09-30 19:45:23 +0200

    add btc price

--- src/ElasticBundle/Controller/AboutController.php
+++ src/ElasticBundle/Controller/AboutController.php
@@ -17,9 +17,18 @@ class AboutController extends AbstractBaseController
         $state = $elasticManager->getState(true);
         $requestCount = $this->em->getRepository('ElasticBundle:Request')->findLast(1);
 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://bittrex.com/api/v1.1/public/getticker?market=btc-xel');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        $data = json_decode($response);
+        $btc_price = number_format($data->result->Last, 8, '.', '');
+
         return $this->render('ElasticBundle:About:index.html.twig',[
             'blockchainStatus' => $blockchainStatus,
             'state' => $state,
+            'btc_price' => $btc_price,
             'requestCount' => $requestCount
         ]);
     }
